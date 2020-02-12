c1a9a68a12b97ca685b112050ed1354c5eab52be
Stephen Moody <darilligames@gmail.com>
2020-02-12 16:02:43 +0000

    hello kerry :)

---
+++ env_copy.py
@@ -0,0 +1,8 @@
+import os
+
+print("Loaded Environmental Variable")
+
+os.environ.setdefault("Super_Secret", "CormacIsLife")
+
+os.environ.setdefault("LIVE_WEBSITE", "www.codeinstitute.net/")
+os.environ.setdefault("DATAbsdfbsdf", "www.sofdsfkghdfgkjdhfgkjdfygudfgdfgbdfjgnkj.net/sdjkfhskjdghdfjkgh/ksdjhfsdjkfh/sfdkjfhsdjkfh/")
